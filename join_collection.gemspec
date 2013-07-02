6b9b12b294013821b47477b6131b337a14d3a79e
Mason Chang <changmason@gmail.com>
2013-07-02 16:41:44 +0800

    initial commit

---
+++ join_collection.gemspec
@@ -0,0 +1,24 @@
+# coding: utf-8
+lib = File.expand_path('../lib', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+require 'join_collection'
+
+Gem::Specification.new do |spec|
+  spec.name          = "join_collection"
+  spec.version       = JoinCollection::VERSION
+  spec.authors       = ["Mason Chang"]
+  spec.email         = ["changmason@gmail.com"]
+  spec.description   = %q{Joining mongoid docs with specified relation}
+  spec.summary       = %q{Joining mongoid docs with specified relation}
+  spec.homepage      = ""
+  spec.license       = "MIT"
+
+  spec.files         = `git ls-files`.split($/)
+  spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
+  spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
+  spec.require_paths = ["lib"]
+
+  spec.add_development_dependency "bundler", "~> 1.3"
+  spec.add_development_dependency "mongoid"
+  spec.add_development_dependency "rake"
+end
---
+++ lib/join_collection.rb
@@ -0,0 +1,79 @@
+class JoinCollection
+
+  VERSION = "0.0.1"
+
+  attr_reader :source_objects
+  attr_accessor :join_type, :singular_target, :plural_target
+
+  def initialize(collection)
+    @source_objects = collection
+  end
+
+  def join_to(target, target_class, options)
+    self.join_type = :join_to
+    fk, pk, delegate_if, delegate_fields = extract_options(target, options)
+
+    source_fks     = source_objects.map(&fk).compact
+    target_objects = target_class.where(pk.in => source_fks).to_a
+
+    mapper = target_objects.group_by(&pk)
+    mapper.default = []
+    join_data(mapper, fk, delegate_if, delegate_fields)
+  end
+
+  def join_one(target, target_class, options)
+    self.join_type = :join_one
+    join_many(target, target_class, options)
+  end
+
+  def join_many(target, target_class, options)
+    self.join_type = :join_many unless self.join_type == :join_one
+    fk, pk, delegate_if, delegate_fields = extract_options(target, options)
+
+    source_pks     = source_objects.map(&pk).compact
+    target_objects = target_class.where(fk.in => source_pks).to_a
+
+    mapper = target_objects.group_by(&fk)
+    mapper.default = []
+    join_data(mapper, pk, delegate_if, delegate_fields)
+  end
+
+  private
+
+  def extract_options(target, options)
+    self.singular_target = target.to_s.singularize.to_sym
+    self.plural_target   = target.to_s.pluralize.to_sym
+
+    relation = options[:relation]
+    fk = relation.keys.first
+    pk = relation.values.first
+
+    delegation = options[:delegation]
+    if_block = delegation[:if] || lambda { |x| true }
+    fields = delegation[:fields] || []
+
+    return fk, pk, if_block, fields
+  end
+
+  def join_data(mapper, key, delegate_if, delegate_fields)
+    join_type       = self.join_type
+    singular_target = self.singular_target
+    plural_target   = self.plural_target
+
+    source_objects.each do |doc|
+      target_objects = mapper[doc[key]]
+      target_object  = (target_objects.find &delegate_if) || target_objects.first
+
+      delegate_fields.each do |field|
+        case field
+        when singular_target
+          doc[singular_target] = target_object unless join_type == :join_many
+        when plural_target
+          doc[plural_target] = target_objects if join_type == :join_many
+        else
+          doc["#{singular_target}_#{field}"] = target_object.try(field)
+        end
+      end
+    end
+  end
+end
